af1bb56a81b9d6759e4891f92cf1c66ba3ea7e31
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-28 18:56:00 +0000

    Setup server

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const { readFileSync } = require('fs')
+const path = require('path')
+
+
+const PORT = process.env.PORT ?? 4000
+
+const typeDefs = readFileSync(path.join(__dirname, './schema.graphql'), 'utf-8')
+
+const resolvers = require('./resolvers')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  path: '/',
+})
+
+mongoose.connect(process.env.DATABASE_URL, () => {
+  console.log('✅ Connected to db!')
+})
+
+server.listen(PORT, () => {
+  console.log('🚀 Server up an running')
+})
