916a22adc758492f033cd38cb7a704de3a2e106f
joey364 <71462893+joey364@users.noreply.github.com>
2021-10-29 15:57:25 +0000

    Implement createBlogPost mutation

--- src/resolvers/mutations/createBlogPost.js
+++ src/resolvers/mutations/createBlogPost.js
@@ -1,8 +1,16 @@
 const createBlogPost = {
   description: 'Create a blog post',
-  resolve: () => {
-    // TODO implement createBlogPost resolver
-    return null
+  resolve: async (parent, args, { models }) => {
+    //get user id
+    const user = await models.User.findOne({ name: 'Alex' })
+    // create new post
+    const newPost = new models.BlogPost({
+      postText: args.postText,
+      postedBy: user._id,
+    })
+    await newPost.save()
+
+    return newPost
   },
 }
 
