e6ea91eeb5668f14f72885b2378c1475d5c8bf76
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-18 00:17:37 +0000

    pass userId through server context

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const path = require('path')
 const models = require('./models')
 
 const PORT = process.env.PORT ?? 4000
+const { DATABASE_URL } = process.env
 
 const typeDefs = readFileSync(path.join(__dirname, './schema.graphql'), 'utf-8')
 
 const resolvers = require('./resolvers')
+const { getUserId } = require('./utils')
 
 const server = new ApolloServer({
   typeDefs,
@@ -21,10 +23,11 @@ const server = new ApolloServer({
     ...req,
     ...res,
     models,
+    userId: getUserId(models),
   }),
 })
 
-mongoose.connect(process.env.DATABASE_URL, () => {
+mongoose.connect(DATABASE_URL, () => {
   console.log('✅ Connected to db!')
 })
 
