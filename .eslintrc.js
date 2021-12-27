5b14eeff301eda106748c35dba6c5bbe70a82ffb
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-27 18:43:09 +0000

    update eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'node', 'prettier'],
+  plugins: ['prettier', 'node'],
   parserOptions: {
     ecmaVersion: 13,
   },
   rules: {
+    'import/no-commonjs': 0,
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'prettier/prettier': 2,
     semi: 0,
-    'no-console': 1,
   },
-};
+}
