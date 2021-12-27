2d15be44fb068051b28c30391cbe2f7565a53769
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-27 18:10:16 +0000

    update resolvers on Comment type

--- src/resolvers/Comment.js
+++ src/resolvers/Comment.js
@@ -1,23 +1,29 @@
 const post = {
   description: 'Return post being replied to',
   resolve: async (parent, _args, { models }) => {
-    console.log('Comment parent', parent)
-    const postParent = await models.BlogPost.find({ _id: parent._id })
+    // comment document should have a post field
+    // that matches a particular post
+    const postParent = await models.BlogPost.findOne({ _id: parent.post })
     return postParent
   },
 }
 
 const replies = {
-  description: 'return replies for post',
+  description: 'return replies to comment',
   resolve: async (parent, _args, { models }) => {
-    const commentParent = models.Reply.find({ _id: parent._id })
+    // comment document should have an field
+    // that matches a reply document
+    const commentParent = models.Reply.find({ comment: parent._id })
     return commentParent
   },
 }
+
 const postedBy = {
   description: 'Return user info for comment',
+  // comment document should have an id field
+  // matching a user id
   resolve: async (parent, _args, { models }) => {
-    const postAuthor = await models.User.find({ _id: parent.postedBy._id })
+    const postAuthor = await models.User.findOne({ _id: parent.postedBy._id })
     return postAuthor
   },
 }
