31c3103dd8eb298afe85c89e00d47e8695d37ec2
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-27 16:16:47 +0000

    update resolvers to match in Reply model

--- src/resolvers/Reply.js
+++ src/resolvers/Reply.js
@@ -1,20 +1,20 @@
-const post = {
-  description: 'Return post being replied to',
+const comment = {
+  description: 'Return comment being replied to',
   resolve: async (parent, _args, { models }) => {
-    const postParent = await models.BlogPost.find({ _id: parent._id })
-    return postParent
+    const commentParent = await models.Comment.findOne({ comment: parent._id })
+    return commentParent
   },
 }
 
 const postedBy = {
-  description: 'Return post being replied to',
+  description: 'Return user replying',
   resolve: async (parent, _args, { models }) => {
-    const postAuthor = await models.User.find({ _id: parent.postedBy._id })
+    const postAuthor = await models.User.findOne({ _id: parent.postedBy._id })
     return postAuthor
   },
 }
 
 module.exports = {
-  post,
+  comment,
   postedBy,
 }
